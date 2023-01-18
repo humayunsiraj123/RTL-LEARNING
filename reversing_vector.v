2d84b9354f79c22fcd812ae594831559d139f26d
humayunsiraj123 <58903281+humayunsiraj123@users.noreply.github.com>
2023-01-18 14:19:33 +0500

    Create reversing_vector.v

---
+++ reversing_vector.v
@@ -0,0 +1,24 @@
+module top_module( 
+    input [7:0] in,
+    output  [7:0] out
+);
+    assign out={in[7],in[6],in[5],in[4],in[3],in[2],in[1],in[0]};
+    integer i=0;
+    integer max=7;
+    always @(*)
+        begin
+            for(i=0;i<8;i++)begin
+                 //out[max-i]=in[i];
+                out[i]=in[max-i];
+        
+            end
+        end
+   
+    generate 
+        genvar x;
+        for (x=0;x<8;x=x+1)
+            begin
+            assign out[7-i]=in[i];
+        end
+    endgenerate
+                endmodule
